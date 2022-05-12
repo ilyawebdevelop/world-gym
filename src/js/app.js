10e8f6d370ad246621dd01c2557978325ac34834
ilyawebdevelop <ilia966@tut.by>
2022-05-13 01:42:19 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import * as flsFunctions from "./modules/functions.js";
+// import './components.js';
+
+flsFunctions.isWebp();
+
+/*
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+const swiper = new Swiper();
+
+*/
