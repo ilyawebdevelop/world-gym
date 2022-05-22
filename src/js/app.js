468fea9b3bc8a7eac135045cd0bdea51ce28fe17
ilyawebdevelop <ilia966@tut.by>
2022-05-22 20:26:47 +0300

    verstka

--- src/js/app.js
+++ src/js/app.js
@@ -9,39 +9,36 @@ Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, Thumbs]);
 
 // Инициализация слайдера intro__slider
 const intoMainSlider = document.querySelector('.intro__slider');
-let mySwiperIntroMain = new Swiper(intoMainSlider, { 	
-  slidesPerView: 1,	
-  spaceBetween: 0, 	
-  autoplay: {
-    delay: 7000,
-  },
-  loop: true,    
+let mySwiperIntroMain = new Swiper(intoMainSlider, {
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
 	navigation: {
-    nextEl: '.intro__navigation-next',
-    prevEl: '.intro__navigation-prev',
-  }
+		nextEl: '.intro__navigation-next',
+		prevEl: '.intro__navigation-prev',
+	}
 });
 
 // Инициализация навигационного слайдера about-main
 const aboutSliderSm = document.querySelector('.about-main__slider-sm');
-let mySwiperAboutMainSm = new Swiper(aboutSliderSm, { 	
-  slidesPerView: 3,	
-  spaceBetween: 15,  
+let mySwiperAboutMainSm = new Swiper(aboutSliderSm, {
+	slidesPerView: 3,
+	spaceBetween: 15,
 	navigation: {
-    nextEl: '.about-main__navigation-next',
-    prevEl: '.about-main__navigation-prev',
-  }
+		nextEl: '.about-main__navigation-next',
+		prevEl: '.about-main__navigation-prev',
+	}
 });
 
 // Инициализация слайдера about-main
 const aboutSliderLg = document.querySelector('.about-main__slider-lg');
-let mySwiperAboutMainLg = new Swiper(aboutSliderLg, { 	
-  slidesPerView: 1,	
-  spaceBetween: 0, 	
+let mySwiperAboutMainLg = new Swiper(aboutSliderLg, {
+	slidesPerView: 1,
+	spaceBetween: 0,
 	navigation: {
-    nextEl: '.about-main__navigation-next',
-    prevEl: '.about-main__navigation-prev',
-  },
+		nextEl: '.about-main__navigation-next',
+		prevEl: '.about-main__navigation-prev',
+	},
 	thumbs: {
 		swiper: mySwiperAboutMainSm,
 	}
@@ -49,54 +46,54 @@ let mySwiperAboutMainLg = new Swiper(aboutSliderLg, {
 
 // Инициализация слайдера team-main__slider
 const teamSliderMain = document.querySelector('.team-main__slider');
-let mySwiperTeamMain = new Swiper(teamSliderMain, { 	
-  slidesPerView: 4,	
-  spaceBetween: 30, 	
-  pagination: {
+let mySwiperTeamMain = new Swiper(teamSliderMain, {
+	slidesPerView: 4,
+	spaceBetween: 30,
+	pagination: {
 		el: '.pagination',
 		clickable: true,
-		type: 'bullets',   
+		type: 'bullets',
 	},
-  loop: true,   
+	loop: true,
 	navigation: {
-    nextEl: '.navigation__arrow-next',
-    prevEl: '.navigation__arrow-prev',
-  },
+		nextEl: '.navigation__arrow-next',
+		prevEl: '.navigation__arrow-prev',
+	},
 });
 
 // Инициализация слайдера gallery
 const gallerySliderMain = document.querySelector('.gallery-slider');
-let mySwiperGalleryMain = new Swiper(gallerySliderMain, { 	
-  slidesPerView: 1,	
-  centeredSlides: true,
-  spaceBetween: 30,  	
-  loop: true,   
+let mySwiperGalleryMain = new Swiper(gallerySliderMain, {
+	slidesPerView: 1,
+	centeredSlides: true,
+	spaceBetween: 30,
+	loop: true,
 	navigation: {
-    nextEl: '.gallery-slider__arrow-right',
-    prevEl: '.gallery-slider__arrow-left',
-  },
+		nextEl: '.gallery-slider__arrow-right',
+		prevEl: '.gallery-slider__arrow-left',
+	},
 });
 
 // Инициализация навигационного слайдера about-main
 const serviceSliderSm = document.querySelector('.service__slider-sm');
-let mySwiperServiceMainSm = new Swiper(serviceSliderSm, { 	
-  slidesPerView: 4,	
-  spaceBetween: 15,  
+let mySwiperServiceMainSm = new Swiper(serviceSliderSm, {
+	slidesPerView: 4,
+	spaceBetween: 15,
 	navigation: {
-    nextEl: '.service__navigation-next',
-    prevEl: '.service__navigation-prev',
-  }
+		nextEl: '.service__navigation-next',
+		prevEl: '.service__navigation-prev',
+	}
 });
 
 // Инициализация слайдера about-main
 const serviceSliderLg = document.querySelector('.service__slider-lg');
-let mySwiperServiceMainLg = new Swiper(serviceSliderLg, { 	
-  slidesPerView: 1,	
-  spaceBetween: 0, 	
+let mySwiperServiceMainLg = new Swiper(serviceSliderLg, {
+	slidesPerView: 1,
+	spaceBetween: 0,
 	navigation: {
-    nextEl: '.service__navigation-next',
-    prevEl: '.service__navigation-prev',
-  },
+		nextEl: '.service__navigation-next',
+		prevEl: '.service__navigation-prev',
+	},
 	thumbs: {
 		swiper: mySwiperServiceMainSm,
 	}
@@ -104,18 +101,18 @@ let mySwiperServiceMainLg = new Swiper(serviceSliderLg, {
 
 // Инициализация слайдера news
 const newsSlider = document.querySelector('.news-slider');
-let mySwiperNews = new Swiper(newsSlider, { 	
-  slidesPerView: 1,	
-  spaceBetween: 0, 	
+let mySwiperNews = new Swiper(newsSlider, {
+	slidesPerView: 1,
+	spaceBetween: 0,
 	pagination: {
 		el: '.pagination',
 		clickable: true,
-		type: 'bullets',   
+		type: 'bullets',
 	},
 	navigation: {
-    nextEl: '.navigation__arrow-next',
-    prevEl: '.navigation__arrow-prev',
-  },
+		nextEl: '.navigation__arrow-next',
+		prevEl: '.navigation__arrow-prev',
+	},
 });
 
 
@@ -132,7 +129,7 @@ window.addEventListener('scroll', () => {
 	if (scrollDistance > lastScrollTop) {
 		header.classList.remove('header--bg');
 		// first.style.marginTop = null;	
-	
+
 	} else {
 		header.classList.add('header--bg');
 		// first.style.marginTop = `${headerHeight}px`;
@@ -140,7 +137,7 @@ window.addEventListener('scroll', () => {
 
 	if (scrollDistance === 0) {
 		header.classList.remove('header--bg');
-		first.style.marginTop = null;	
+		first.style.marginTop = null;
 	}
 	lastScrollTop = scrollDistance;
 });
@@ -202,7 +199,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		tabs.addEventListener('click', (e) => {
 			if (e.target.classList.contains('tabs__btn')) {
 				const tabsPath = e.target.dataset.tabsPath;
-				tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
+				tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') });
 				document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn--active');
 				tabsHandler(tabsPath);
 			}
@@ -214,7 +211,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 				if (previousParent) {
 					let prevActive = previousParent.querySelector('.tabs__btn')
-					tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
+					tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') });
 					prevActive.classList.add('tabs__btn--active');
 
 					let path = prevActive.dataset.tabsPath;
@@ -229,7 +226,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 				if (nextParent) {
 					let nextActive = nextParent.querySelector('.tabs__btn');
-					tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
+					tabsBtn.forEach(el => { el.classList.remove('tabs__btn--active') });
 					nextActive.classList.add('tabs__btn--active');
 
 					let path = nextActive.dataset.tabsPath;
@@ -240,8 +237,21 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 
 	const tabsHandler = (path) => {
-		tabsContent.forEach(el => {el.classList.remove('tabs__content--active')});
+		tabsContent.forEach(el => { el.classList.remove('tabs__content--active') });
 		document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content--active');
 	};
 });
+
+
+// LightGallery
+lightGallery(document.getElementById('lightgallery'), {
+	thumbnail: true,
+	animateThumb: false,
+	showThumbByDefault: false
+});
+
+
+
+
+
 
